699e82725e5ac02fce96a03f8cba0a3f12e92de1
Shashwat Kumar <thegreatshasha@gmail.com>
2015-03-17 14:11:00 +0530

    my branch. all mine

--- server/publications.js
+++ server/publications.js
@@ -1,20 +1,20 @@
 Meteor.startup(function () {
     // Publish the collection. TODO: Only publish part of the collection to which the user has permissions. Also how do we limit data size in meteor? Should be handled with pagination.
-    Meteor.publish('clients', function() {
-      return Clients.find();
-    });
+  Meteor.publish('clients', function() {
+    return Clients.find();
+  });
 
-    Meteor.publish('projects', function() {
-      return Projects.find();
-    })
+  Meteor.publish('projects', function() {
+    return Projects.find();
+  })
 
-    Meteor.publish('lawyers', function(){
-      return Lawyers.find();
-    });
+  Meteor.publish('lawyers', function(){
+    return Lawyers.find();
+  });
 
-    Meteor.publish('courts', function(){
-      return Courts.find();
-    })
+  Meteor.publish('courts', function(){
+    return Courts.find();
+  })
 	
 	Meteor.publish('hearings', function(){
 		return Hearings.find();
@@ -31,5 +31,9 @@ Meteor.startup(function () {
 	Meteor.publish('meetings', function(){
 		return Meetings.find();
 	})
+
+  Meteor.publish('allJobs', function(){
+    return myJobs.find();
+  })
 	
 });

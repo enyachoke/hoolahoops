15b46454881c1005f32a129a403f770a37c7f53f
Rishabh <rishabh@dinasource.com>
2015-03-19 23:11:25 +0530

    drive functions

--- server/config.js
+++ server/config.js
@@ -7,5 +7,29 @@ Meteor.startup(function() {
 
     // // Start the job queue
     // myJobs.startJobs();
+    var test = "5";
+    function test123(){
+    	return '123';
+    }
+
+
+
+    TOKENS = { access_token: 'ya29.OwGtQG1UbLyiBs0ei_YXvDoysM-YEhelqJsMlenhcbQUw0SqJkLbZsNtdXEB5hsxkjOd3VASFR7fTQ',
+token_type: 'Bearer',
+id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjliZDA3Mzc5MjMyYjAyMmMxNWI4OTg3YjU0ZDc5OGRhNGFhMmQwOWIifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwic3ViIjoiMTEyNTY2NjkyMTc3ODA0MDI5NTgwIiwiYXpwIjoiMTA0MjUyNjQ3OTM5MS1tNm4wZG1jOTdlMGM4NGp1Y2o1Z3N1MTNxdG1mNGRvOS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF0X2hhc2giOiJ1Qk5sR2h0QmMwMWpzMWtSdXh4VFR3IiwiYXVkIjoiMTA0MjUyNjQ3OTM5MS1tNm4wZG1jOTdlMGM4NGp1Y2o1Z3N1MTNxdG1mNGRvOS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImlhdCI6MTQyNjc4MzczMywiZXhwIjoxNDI2Nzg3NTEzfQ.sJVWpOzyxowtk8AGhPxTjFU9NRfc35aC6Zf5FsMPlaES9hPhFe4o-R6SWgrBgh-L2L71WmnXpGesgzHxgZaC-MayxqHuse5TSAVuXuLRUz9hd3Ge9c200qdNlKpGjKvHYpJwbYhV6cMuwq7GI4AUKFTKZVlnJmLUhb49oYNxl6M',
+expiry_date: 1426787511725 };
+    GOOGLE = Meteor.npmRequire('googleapis');
+	OAUTH2 = GOOGLE.auth.OAuth2;
+	CLIENT_ID = '1042526479391-m6n0dmc97e0c84jucj5gsu13qtmf4do9.apps.googleusercontent.com';
+	CLIENT_SECRET = 'GF3m9QmVZYKkAHbRgx_SASrz';
+	REDIRECT_URL = 'http://localhost:3000/googleauth';
+	OAUTH2_CLIENT = new OAUTH2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+	SCOPES = [
+		'https://www.googleapis.com/auth/plus.me',
+		'https://www.googleapis.com/auth/calendar',
+		'https://www.googleapis.com/auth/drive'
+	];
+	DRIVE = GOOGLE.drive('v2');
+
 
 });
--- server/methods.js
+++ server/methods.js
@@ -1,42 +1,43 @@
 Meteor.methods({
 	'getGoogleAuthUrl' : function(){
-		 google = Meteor.npmRequire('googleapis');
-		 OAuth2 = google.auth.OAuth2;
-		var CLIENT_ID = '1042526479391-m6n0dmc97e0c84jucj5gsu13qtmf4do9.apps.googleusercontent.com';
-		var CLIENT_SECRET = 'GF3m9QmVZYKkAHbRgx_SASrz';
-		var REDIRECT_URL = 'http://localhost:3000/googleauth';
-		 oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-		https://www.googleapis.com/auth/drive
-		var scopes = [
-		  'https://www.googleapis.com/auth/plus.me',
-		  'https://www.googleapis.com/auth/calendar',
-		  'https://www.googleapis.com/auth/drive'
-		];
+		// google = Meteor.npmRequire('googleapis');
+		// OAuth2 = google.auth.OAuth2;
+		// var CLIENT_ID = '1042526479391-m6n0dmc97e0c84jucj5gsu13qtmf4do9.apps.googleusercontent.com';
+		// var CLIENT_SECRET = 'GF3m9QmVZYKkAHbRgx_SASrz';
+		// var REDIRECT_URL = 'http://localhost:3000/googleauth';
+		// oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+		// // https://www.googleapis.com/auth/drive
+		// var scopes = [
+		//   'https://www.googleapis.com/auth/plus.me',
+		//   'https://www.googleapis.com/auth/calendar',
+		//   'https://www.googleapis.com/auth/drive'
+		// ];
 
-		var url = oauth2Client.generateAuthUrl({
+		var url = OAUTH2_CLIENT.generateAuthUrl({
 		  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-		  scope: scopes // If you only need one scope you can pass it as string
+		  scope: SCOPES // If you only need one scope you can pass it as string
 		});
 		console.log('url :',url);
 		return url;	
 	},
 	'getGoogleAuthToken' : function(code){
-		console.log(code);
-		var google = Meteor.npmRequire('googleapis');
-		var OAuth2 = google.auth.OAuth2;
-		var CLIENT_ID = '1042526479391-m6n0dmc97e0c84jucj5gsu13qtmf4do9.apps.googleusercontent.com';
-		var CLIENT_SECRET = 'GF3m9QmVZYKkAHbRgx_SASrz';
-		var REDIRECT_URL = 'http://localhost:3000/googleauth';
-		var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-		var plus = google.plus('v1');
+		// console.log(code);
+		// var google = Meteor.npmRequire('googleapis');
+		// var OAuth2 = google.auth.OAuth2;
+		// var CLIENT_ID = '1042526479391-m6n0dmc97e0c84jucj5gsu13qtmf4do9.apps.googleusercontent.com';
+		// var CLIENT_SECRET = 'GF3m9QmVZYKkAHbRgx_SASrz';
+		// var REDIRECT_URL = 'http://localhost:3000/googleauth';
+		//var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+		//var plus = google.plus('v1');
 		
 
 		var token = Async.runSync(function(done) {
-        	oauth2Client.getToken(code, function(err, tokens) {
+        	OAUTH2_CLIENT.getToken(code, function(err, tokens) {
 	  		// Now tokens contains an access_token and an optional refresh_token. Save them.
 				if(!err) {
-			    	oauth2Client.setCredentials(tokens);
+			    	OAUTH2_CLIENT.setCredentials(tokens);
 			    	console.log('token fetched',tokens);
+			    	TOKENS = tokens;
 			    	done(err,tokens);
 				}
 			});
@@ -52,44 +53,145 @@ Meteor.methods({
   //     	});
 		
 
-		var drive = google.drive('v2');
+		var drive = GOOGLE.drive('v2');
       	
 
 
       	// insertion example : create folder
-      	var response_drive = Async.runSync(function(done) {
+  //     	var response_drive = Async.runSync(function(done) {
         	
-		 	// handle err and response
-			drive.files.insert({
-				resource: {
-				    title: 'pets',
-				    "mimeType": "application/vnd.google-apps.folder"    
-				},
-				media: {
-					"title": "pets123",
-					mimeType: 'text/plain'
-				},
-				auth: oauth2Client
-			}, function(err, response) {
-				console.log('error:', err, 'inserted:', response.id);
-				done(err, response.id);
+		//  	// handle err and response
+		// 	drive.files.insert({
+		// 		resource: {
+		// 		    title: 'A vs B',
+		// 		    "mimeType": "application/vnd.google-apps.folder"    
+		// 		},
+		// 		// media: {
+		// 		// 	"title": "pets123",
+		// 		// 	mimeType: 'text/plain'
+		// 		// },
+		// 		auth: oauth2Client
+		// 	}, function(err, response) {
+		// 		console.log('error:', err, 'inserted:', response.id);
+		// 		done(err, response.id);
+		// 	});
+		// });
+
+
+    //   	// listing files
+    //   	var response_list_req = Async.runSync(function(done){
+    //   		drive.files.list({
+    //   			'maxResults' : 20,
+				// auth: oauth2Client
+    //   		},function(err, response){
+    //   			done(err, response);
+    //   			console.log('files',err, response);
+    //   		});
+    //   	});
+
+		//FETCH children
+		var response_get_list = Async.runSync(function(done){
+			drive.children.list({
+				folderId: '0B8XNqOrM9GYOX1dWdVRleG1GbmM',
+				auth: OAUTH2_CLIENT
+			},function(err,res){
+				done(err,res);
+				console.log('list',err,res);
 			});
 		});
 
+		
 
-      	// listing files
-      	var response_list_req = Async.runSync(function(done){
-      		drive.files.list({
-      			'maxResults' : 20,
-				auth: oauth2Client
-      		},function(err, response){
-      			done(err, response);
-      			console.log('files',err, response);
-      		});
-      	});
+		//fetch file from id
+		// var file_get_res = Async.runSync(function(done){
+		// 	drive.files.get({
+		// 		fileId: '1AcbV9LuRhEcve3OGsUm6oySJaOHeeJwWWjVLCkbbLcU',
+		// 		auth: OAUTH2_CLIENT
+		// 	},function(err,res){done(err,res)});
+		// });
 
-		return response_drive;
+		// var response_list_req = Async.runSync(function(done){
+  //     		drive.files.list({
+  //     			'q' : 'pets',
+		// 		auth: OAUTH2_CLIENT
+  //     		},function(err, response){
+  //     			done(err, response);
+  //     			console.log('files',err, response);
+  //     		});
+  //     	});
 
+
+		return response_get_list;
+
+	},
+	// 'getFolderId' : function(){
+	// 	//listing files
+	// 	OAUTH2_CLIENT.setCredentials(TOKENS);
+	// 	var drive = GOOGLE.drive('v2');
+ //      	var response_list_req = Async.runSync(function(done){
+ //      		drive.files.list({
+ //      			maxResults : 5,
+ //      			'q' : 'pets',
+	// 			auth: OAUTH2_CLIENT
+ //      		},function(err, response){
+ //      			done(err, response);
+ //      			console.log('files',err, response);
+ //      		});
+ //      	});
+
+	// 	return response_list_req;
+	// },
+	'insertFolder' : function (title){
+		OAUTH2_CLIENT.setCredentials(TOKENS);
+      	var response = Async.runSync(function(done) {
+			DRIVE.files.insert({
+				resource: {
+				    title: title,
+				    "mimeType": "application/vnd.google-apps.folder"    
+				},
+				auth: OAUTH2_CLIENT
+			}, function(err, response) {
+				console.log('error:', err, 'inserted:', response);
+				done(err, response);
+			});
+		});
+
+		return response;
+	},
+	'getFilesByFolderId' : function(folderId) {
+		var items = [];
+		OAUTH2_CLIENT.setCredentials(TOKENS);
+		//FETCH children
+		var response = Async.runSync(function(done){
+			DRIVE.children.list({
+				folderId: folderId,
+				auth: OAUTH2_CLIENT
+			},function(err,res){
+				done(err,res);
+				console.log('list',err,res);
+			});
+		});
+		if (!response.error){
+			response.result.items.forEach(function(item){
+				item = Meteor.call('getFileById',item.id);
+				items.push(item);
+			});
+		}
+	
+		return items;
+	},
+	'getFileById' : function(fileId){
+		//fetch file from id
+		OAUTH2_CLIENT.setCredentials(TOKENS);
+		var response = Async.runSync(function(done){
+			DRIVE.files.get({
+				fileId: fileId,
+				auth: OAUTH2_CLIENT
+			},function(err,res){done(err,res)});
+		});
+
+		return response;
 	}
+
 });
 

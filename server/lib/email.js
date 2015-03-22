dcd001430b8f87e2225a88a0a5279dc4cce9b954
Shashwat Kumar <thegreatshasha@gmail.com>
2015-03-22 21:05:10 +0530

    added links in mandrill

--- server/lib/email.js
+++ server/lib/email.js
@@ -1,7 +1,10 @@
 addEmailReminders = function(doc, template) {
 	_.each(doc.reminders, function(reminder){
 		// TODO: Group emails by date to save mandrill bandwidth
-		var job = myJobs.createJob('addEmail', {'name': 'Send Email', 'template': template, 'merge_vars': toMandrillArray(doc), 'to': reminder.email});
+		//TODO: Do this using iron router. Use a transform function here with collection helpers
+		doc.url = Meteor.absoluteUrl() + template + '/' + doc._id;
+		var merge_vars = toMandrillArray(doc);
+		var job = myJobs.createJob('addEmail', {'name': 'Send Email', 'template': template, 'merge_vars': merge_vars, 'to': reminder.email});
 		var delayMilliSeconds = Math.abs(reminder.date - new Date());
 		job.retry({retries: 4, wait: 2*60*1000});
 		job.delay(delayMilliSeconds);

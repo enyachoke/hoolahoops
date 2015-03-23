045c1889b786a177e6c4f7a6274d2a3a12d99e78
Rishabh <rishabh@dinasource.com>
2015-03-23 10:09:40 +0530

    hearing edit

--- client/templates/hearings/edit.js
+++ client/templates/hearings/edit.js
@@ -6,5 +6,31 @@ Template.hearingEdit.events({
       Hearings.update(this._id, {$set: formJSON}, function(error){
         Router.go('hearingDetails', {_id: currentId});
       });
-    }
-  })
+    },
+    "change #attended_by_type select": function(evt) {
+		  var newValue = $(evt.target).val();
+		  Session.set("attended_by", newValue);
+	}
+});
+
+
+
+
+Template.hearingEdit.helpers({
+
+	'amount_by_type' : function(){
+		//debugger;
+		// var attended_by = Session.get("attended_by");
+		// var amt;
+		// if ( attended_by == 'Partner' ) {
+		// 	amt = this.project.bill_hearing_partner;
+		// }else if ( attended_by == 'Sr Associate' ) {
+		// 	amt = this.project.bill_hearing_sr_associate;
+		// }else if ( attended_by == 'Associate' ) {
+		// 	amt = this.project.bill_hearing_associate;
+		// }
+		// Session.set('amount_by_type',amt);
+		// return Session.get('amount_by_type');
+		return this.bill_amt;
+	}
+});

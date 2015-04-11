34565a3dba701518bef05eaa9ea3bee95f869816
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-04-11 12:45:49 +0530

    Creates a new folder if doesn't exists for the people signed in with google

--- server/config.js
+++ server/config.js
@@ -32,18 +32,6 @@ Meteor.startup(function() {
 	DRIVE = GOOGLE.drive('v2');
 
 	Kadira.connect('JuJNAjRAZr7Ag3wnf', 'f5e55ed4-9b38-447d-9812-be859f771c35')
-
-	//create root folder
-	var shortId = Meteor.npmRequire('shortid');
-	var root_folder_title = shortId.generate();
-	if ( RootFolders.find().fetch().length == 0 ){
-		Meteor.call('insertFolder',root_folder_title,function(err,res){
-			console.log(err, res);
-			RootFolders.insert({title : root_folder_title, id : res.result.id})
-    	});
-	}
-    
-
 });
 
 

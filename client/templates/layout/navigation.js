1ae2b78f1995cc593a614ff6668eabdf06485517
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-05-15 05:19:00 +0530

    Fixes the navigation bar dropdown list, implements own logout and change password functionality (with modal)

---
+++ client/templates/layout/navigation.js
@@ -0,0 +1,24 @@
+Template.navigation.events({
+	'click #logout': function(){
+		Meteor.logout();
+	},
+	'click #change_password': function(){
+		$("#success_message, #error_message").text("");
+		if($("#new_password").val().length>=6){
+			Meteor.loginWithPassword(Meteor.user().username, $("#current_password").val(), function(err){
+				if(err)
+					$("#error_message").text("Incorrect password");
+				else
+					if($("#new_password").val()==$("#confirm_new_password").val()){
+						Accounts.changePassword($("#current_password").val(), $("#new_password").val());
+						$("#success_message").text("Password changed");
+						$("#current_password, #confirm_new_password, #new_password").val("");
+					}
+					else
+						$("#error_message").text("Passwords don't match");
+			})
+		}
+		else
+			$("#error_message").text("Password must be at least 6 characters long");
+	}
+});

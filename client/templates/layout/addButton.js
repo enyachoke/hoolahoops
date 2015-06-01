86f41b967aa2e4856933dd4fccf59fa2dfa80e1c
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-06-01 09:37:59 +0530

    Adds universal plus button with animation and button labeling

---
+++ client/templates/layout/addButton.js
@@ -0,0 +1,34 @@
+Template.addButton.rendered = function () {
+    var template = this;
+    $(document).off('.fixedActionBtn', '.fixed-action-btn');
+    var opened = false;
+    $(document).on('click.fixedActionBtn', '.fixed-action-btn', function () {
+        var elements = $(template.findAll('ul li'));
+        if (!opened) {
+            $(template.find('.fixed-action-btn .large.mdi-content-add'))
+                .switchClass("mdi-content-add", "mdi-navigation-close");
+            elements.velocity(
+                {scaleY: ".4", scaleX: ".4", translateY: "40px"},
+                {duration: 0});
+
+            var time = 0;
+            elements.reverse().each(function () {
+                $(this).velocity(
+                    {opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+                    {duration: 80, delay: time});
+                time += 40;
+            });
+            opened = true;
+        }
+        else {
+            var time = 0;
+            elements.velocity("stop", true);
+            $(template.find('.fixed-action-btn .large.mdi-navigation-close'))
+                .switchClass("mdi-navigation-close", "mdi-content-add");
+            elements.velocity(
+                {opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+                {duration: 80});
+            opened = false;
+        }
+    });
+}
